8fab5bbf66e0cc135ad55b6d4d3452d1fab9f647
dallin <dallin@djackson.dev>
2026-03-18 15:28:54 -0600

    perchance the fix we've all been waiting for

--- src/metrics.js
+++ src/metrics.js
@@ -268,7 +268,7 @@ async function sendMetricToGrafana(metrics) {
       method: "POST",
       body: JSON.stringify(body),
       headers: {
-        Authorization: `Basic ${Buffer.from(`${config.metrics.accountId}:${config.metrics.apiKey}`).toString("base64")}`,
+        Authorization: `Bearer ${config.metrics.accountId}:${config.metrics.apiKey}`,
         "Content-Type": "application/json",
       },
     });
